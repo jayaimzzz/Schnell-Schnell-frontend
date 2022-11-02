a5495eeb81ed0b74a59ea144d76e880b21741ca6
jayaimzzz <jameshorton@gmail.com>
2022-11-02 15:49:33 -0400

    changed to use formBuilder. attempted http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,9 @@ import { LoginFormComponent } from './login-form/login-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-form',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
+  form: FormGroup;
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    // TODO add validators to these form fields such as required and username must be an email
+    this.form = this.fb.group({
+      username: [''],
+      password: [''],
+    });
+   }
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  submitForm() {
+    const formData: any = new FormData();
+    formData.append('username', this.form.get('username')?.value);
+    formData.append('password', this.form.get('password')?.value);
+    // TODO move api base url to env var
+    this.http.post('http://localhost:8080/login', formData).subscribe(
+      (response) => console.log(response), 
+      (error) =>console.log(error)
+    )
   }
 
 }
+
+
+
